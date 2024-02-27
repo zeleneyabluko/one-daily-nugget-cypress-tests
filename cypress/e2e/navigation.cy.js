88a16a84015fd9e270b384819fe44552103e3c28
svitlanaD <svitlana.dmytrenko@expercast.com>
2024-02-27 21:45:31 +0200

    user can open an issue view - test

--- cypress/e2e/navigation.cy.js
+++ cypress/e2e/navigation.cy.js
@@ -8,10 +8,18 @@ describe('User can navigate through the pages', () => {
   })
   
 
-  it('There are three issue cards on the main page', () => {
-    cy.react('CardStyledLink').should('have.length', '3');
+  it('User can open an issue view', () => {
+    cy.react('CardStyledLink').should('have.length', '3')
+    .then(($issues) => {
+      const items = $issues.toArray();
+      const random = Math.floor(Math.random() * 3); 
+      return items[random];  
+    })
+    .click();
+
+
   });
 
-  it('')
+  
 
 })
