792d0b2c5214cc6fa00cf10a42ae3ec7cfab957c
svitlanaD <svitlana.dmytrenko@expercast.com>
2024-02-27 14:44:27 +0200

    config update

--- cypress.config.js
+++ cypress.config.js
@@ -1,34 +1,4 @@
-const { defineConfig } = require('cypress');
-
-module.exports = defineConfig({
-  video: false,
-  screenshotOnRunFailure: false,
-  env: {
-    'cypress-react-selector': {
-      root: '#__cy_root',
-    },
-  },
-  e2e: {
-    setupNodeEvents() {},
-    specPattern: 'cypress/e2e/**/*.cy.{js,ts,jsx,tsx}',
-    excludeSpecPattern: ['**/__snapshots__/*', '**/__image_snapshots__/*'],
-    baseUrl: "https://onedailynugget.com",
-  },
-  component: {
-    setupNodeEvents() {},
-    specPattern: 'cypress/component/**/*.cy.{js,ts,jsx,tsx}',
-    excludeSpecPattern: ['**/__snapshots__/*', '**/__image_snapshots__/*'],
-    devServer: {
-      framework: 'create-react-app',
-      bundler: 'webpack',
-    },
-  },
-  
-});
- 
- 
- 
- /*const { defineConfig } = require("cypress");
+const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
@@ -37,4 +7,4 @@ module.exports = defineConfig({
     },
     baseUrl: "https://onedailynugget.com",
   },
-}); */
+}); 
