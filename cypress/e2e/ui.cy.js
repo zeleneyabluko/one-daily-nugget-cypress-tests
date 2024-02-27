deb0fb88d8122d170a86e48125c2e6871bc8545b
svitlanaD <svitlana.dmytrenko@expercast.com>
2024-02-27 12:50:01 +0200

    tests - unsuccessful email submitting

--- cypress/e2e/ui.cy.js
+++ cypress/e2e/ui.cy.js
@@ -1,16 +1,33 @@
 import { HomePage } from "../pages/homePage"
 
-describe('Home page opens successfully', () => {
-  it('Home page opens successfully', () => {
-    const home = new HomePage();
-    home.visit();
+describe('User can navigate through the site', () => {
+  beforeEach(() => {
+     cy.visit('/');
     cy.acceptCookies();
-    })
+  })
+  
+
   it ('Can submit the email', () => {
-    typeEmail('email@email.com');
-    togglePrivacyPolicyCheckbox();
-    clickSubmit();
+    const home = new HomePage();
+    home.typeEmail('email@email.com');
+    home.togglePrivacyPolicyCheckbox();
+    home.clickSubmit();
+    cy.get('h1').should('contain', 'Thank you for signing up!');
     })
+  
+  it ('Unable to submit invalid email address', () => {
+    const home = new HomePage();
+    home.typeEmail('fd');
+    home.clickSubmit();
+    cy.get('span').should('contain', 'Please enter a valid email address');
+  })
+
+  it ('Cannot submit the email without accepting the privacy policy', () => {
+    const home = new HomePage();
+    home.typeEmail('email@email.com');
+    home.clickSubmit();
+    cy.get('span').should('contain', 'Please agree with our Privacy Policy before proceeding');
+  })
   
  
 
