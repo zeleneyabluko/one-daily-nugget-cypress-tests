4eaa2d1edd6ced4e0335a93fd3e7b3af445a32dd
svitlanaD <svitlana.dmytrenko@expercast.com>
2024-02-28 13:19:51 +0200

    opening About page, click on Archive link

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -8,6 +8,7 @@ Cypress.Commands.add('submitSubscriberEmail', () => {
 
 Cypress.Commands.add('checkSignupSuccessPage', () => {
     cy.url().should('contain', '/signup-success');
+    cy.get('h1').should('contain', 'Thank you for signing up!');
 })
 
 Cypress.Commands.add('typeSubscriberEmail', (email) => {
@@ -17,9 +18,21 @@ cy.get("input[type='email']").type(email);
 Cypress.Commands.add('togglePrivacyPolicyCheckbox', () => {
     cy.get("input[type='checkbox']").click();
 })
+
+Cypress.Commands.add('openAboutPage', () => {
+    cy.get('a').contains('About').click({force:true});
+})
+
+Cypress.Commands.add('clickArchiveLinkInHeader', () => {
+    cy.get('a').contains('Archive').click({force:true});
+})
+
+Cypress.Commands.add('checkArchiveModal', () => {
+    cy.react('ModalContent').should('be.visible');
+})
 /*TODO:
-command 'submit email' (available both from issue view and home view and About page)
-open About page (home page, issue view)
+
+
 Archive (home page, issue view), 
 click logo to return home
 */
