029ad224e9d274579fc9a2afd5d346023e4d7774
svitlanaD <svitlana.dmytrenko@expercast.com>
2024-02-28 08:57:22 +0200

    added some todos

---
+++ cypress/pages/issueView.js
@@ -0,0 +1,8 @@
+export class IssueView {
+    /*
+Todo:
+
+elements: issue header, date, text, source link
+methods: all actions will be available in commands
+    */
+}
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,3 +1,9 @@
 Cypress.Commands.add('acceptCookies', () => {
 cy.get('button').contains('Accept').click();
 });
+/*TODO:
+command 'submit email' (available both from issue view and home view and About page)
+open About page (home page, issue view)
+Archive (home page, issue view), 
+click logo to return home
+*/
