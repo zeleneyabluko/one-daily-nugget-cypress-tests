3d314450b68af809ac911082b13b0491be55f145
svitlanaD <svitlana.dmytrenko@expercast.com>
2024-03-01 12:26:36 +0200

    first email test

--- cypress/e2e/emails.cy.js
+++ cypress/e2e/emails.cy.js
@@ -1,16 +1,23 @@
-import Chance from "chance"
+import Chance from "chance";
+import Mailslurp from 'cypress-mailslurp';
 
-describe('User receives confirmation and welcome emails', () => {
-    beforeAll(() => {
-    
+before(async () => {
+    const apiKey = 'fc15634ba596d29067409244c7ebed77de4cffdab60ba1ca449c088ace31275c';
+    cy.mailslurp({ apiKey: apiKey });
+    const inbox = await mailslurp.inboxController.createInbox({});
+   
     })
 
-    beforeEach(() => {
-    
-  })
-  
+describe('User receives confirmation and welcome emails', () => {
+
   it ('User receives the confirmation email upon registration', () => {
-    
+    cy.visit('/');
+    /* cy.waitForReact(1000, '#__next'); 
+    cy.acceptCookies();
+    cy.typeSubscriberEmail();
+    cy.togglePrivacyPolicyCheckbox();
+    cy.submitSubscriberEmail();
+    cy.checkSignupSuccessPage(); */
   })
 
   it ('When the user clicks on the confirmation link, the confirmation page is presented', () => {
