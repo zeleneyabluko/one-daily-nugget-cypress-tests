e20b97411a9484cc8ba5bd198396dc276ee47bc2
svitlanaD <svitlana.dmytrenko@expercast.com>
2024-03-01 16:12:21 +0200

    extracting mailurslurp API key to config

--- cypress.config.js
+++ cypress.config.js
@@ -7,4 +7,9 @@ module.exports = defineConfig({
     },
     baseUrl: "https://onedailynugget.com",
   },
+  
+    "env": {
+      "MAILSLURP_API_KEY": 'fc15634ba596d29067409244c7ebed77de4cffdab60ba1ca449c088ace31275c'
+    }
+  
 }); 
--- cypress/e2e/emails.cy.js
+++ cypress/e2e/emails.cy.js
@@ -1,23 +1,42 @@
 import Chance from "chance";
 import Mailslurp from 'cypress-mailslurp';
 
-before(async () => {
-    const apiKey = 'fc15634ba596d29067409244c7ebed77de4cffdab60ba1ca449c088ace31275c';
-    cy.mailslurp({ apiKey: apiKey });
-    const inbox = await mailslurp.inboxController.createInbox({});
-   
+before(() => {
+    //const apiKey = 'fc15634ba596d29067409244c7ebed77de4cffdab60ba1ca449c088ace31275c';
+    //cy.wrap(apiKey).as('apiKey');
+    
+    cy.visit('/');
+    cy.waitForReact(1000, '#__next'); 
+    cy.acceptCookies();
+
     })
 
 describe('User receives confirmation and welcome emails', () => {
 
   it ('User receives the confirmation email upon registration', () => {
-    cy.visit('/');
-    /* cy.waitForReact(1000, '#__next'); 
-    cy.acceptCookies();
-    cy.typeSubscriberEmail();
+    cy.mailslurp()
+    .then(mailslurp => mailslurp.createInbox())
+    .then(inbox => {
+        cy.wrap(inbox).its('id').as('inboxId');
+        cy.wrap(inbox).its('emailAddress').as('emailAddress');
+        });
+    cy.get('@emailAddress').then((email) => cy.typeSubscriberEmail(email));
     cy.togglePrivacyPolicyCheckbox();
-    cy.submitSubscriberEmail();
-    cy.checkSignupSuccessPage(); */
+        //cy.submitSubscriberEmail();
+        //cy.checkSignupSuccessPage(); 
+    
+   
+   /* cy.get('@emailAddress').then((email) => cy.typeSubscriberEmail(email));
+    cy.togglePrivacyPolicyCheckbox();
+    //cy.submitSubscriberEmail();
+    //cy.checkSignupSuccessPage(); 
+    cy.get('@apiKey')
+    .then(apiKey => cy.mailslurp({ apiKey: apiKey }))
+    .then(mailslurp => mailslurp.waitForLatestEmail(inboxId, 60000, true))
+    */
+    
+      
+
   })
 
   it ('When the user clicks on the confirmation link, the confirmation page is presented', () => {
