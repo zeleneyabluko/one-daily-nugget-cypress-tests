21df6c3911fccfcc283981f3565eedb42c3e4ae2
svitlanaD <svitlana.dmytrenko@expercast.com>
2024-03-01 12:01:36 +0200

    added templates for email tests

---
+++ cypress/e2e/emails.cy.js
@@ -0,0 +1,24 @@
+import Chance from "chance"
+
+describe('User receives confirmation and welcome emails', () => {
+    beforeAll(() => {
+    
+    })
+
+    beforeEach(() => {
+    
+  })
+  
+  it ('User receives the confirmation email upon registration', () => {
+    
+  })
+
+  it ('When the user clicks on the confirmation link, the confirmation page is presented', () => {
+    // https://onedailynugget.com/email-confirmation
+  })
+
+  it ('User receives the welcome email after confirming the email address', () => {
+    
+  })
+
+})
